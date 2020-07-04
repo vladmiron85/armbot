c7bc7cb4113702f16be89145ca988a2e22f1e04a
vlad <vladislav.mironov@gmail.com>
2020-07-04 14:33:22 +0300

    Add project with urdf of armbot

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/vlad/catkin_ws/src
+# Build directory: /home/vlad/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("learning_tf")
+subdirs("armbot_description")
+subdirs("armbot_moveit_config")
---
+++ catkin_ws/build/armbot_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vlad/catkin_ws/src/armbot_moveit_config
+# Build directory: /home/vlad/catkin_ws/build/armbot_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
